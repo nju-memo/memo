ad92a450dfd9d5d6d2c2e7906a865b211065ed01
tinker <tinker19981@hotmail.com>
2017-09-11 20:26:50 +0800

    initial commit

---
+++ app/src/main/java/edu/nju/memo/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.nju.memo
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
